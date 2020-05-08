094b9bf1563ccb6c2f1e6aa312fbc15e7856f2f4
Yuta Ushijima <register@yuta-u.com>
2020-05-08 23:33:48 +0900

    feat: Dockerfileの設定ファイル群を修正

--- config/puma.rb
+++ config/puma.rb
@@ -37,4 +37,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{diary_app_root}/tmp/sockets/puma.sock"
+bind "unix:#{app_root}/tmp/sockets/puma.sock"
